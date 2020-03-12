2935e6b1926cb7dbcff4b4bcc65f1dc8822cbb52
Yujian95 <clj9509@163.com>
2020-03-13 01:02:26 +0800

    refactor(power): update role api..

--- src/main/java/cn/yujian95/hospital/controller/PowerRoleController.java
+++ src/main/java/cn/yujian95/hospital/controller/PowerRoleController.java
@@ -4,7 +4,9 @@ import cn.yujian95.hospital.common.api.CommonPage;
 import cn.yujian95.hospital.common.api.CommonResult;
 import cn.yujian95.hospital.dto.param.PowerRoleParam;
 import cn.yujian95.hospital.dto.param.StatusParam;
+import cn.yujian95.hospital.entity.PowerMenu;
 import cn.yujian95.hospital.entity.PowerPermission;
+import cn.yujian95.hospital.entity.PowerResource;
 import cn.yujian95.hospital.entity.PowerRole;
 import cn.yujian95.hospital.service.IPowerRoleService;
 import io.swagger.annotations.Api;
@@ -144,6 +146,64 @@ public class PowerRoleController {
         }
 
         if (roleService.updateStatus(id, param)) {
+            return CommonResult.success();
+        }
+
+        return CommonResult.failed("服务器错误，请联系管理员！");
+    }
+
+
+    @ApiOperation("获取角色相关菜单")
+    @RequestMapping(value = "/menu/{roleId}", method = RequestMethod.GET)
+    public CommonResult<List<PowerMenu>> listRoleMenu(@PathVariable Long roleId) {
+
+        if (!roleService.count(roleId)) {
+            return CommonResult.validateFailed("不存在，该角色编号！");
+        }
+
+
+        List<PowerMenu> roleList = roleService.listMenu(roleId);
+
+        return CommonResult.success(roleList);
+    }
+
+    @ApiOperation("获取角色相关资源")
+    @RequestMapping(value = "/resource/{roleId}", method = RequestMethod.GET)
+    public CommonResult<List<PowerResource>> listRoleResource(@PathVariable Long roleId) {
+
+        if (!roleService.count(roleId)) {
+            return CommonResult.validateFailed("不存在，该角色编号！");
+        }
+
+        List<PowerResource> roleList = roleService.listMenuResource(roleId);
+        return CommonResult.success(roleList);
+    }
+
+    @ApiOperation("给角色分配菜单")
+    @RequestMapping(value = "/allocMenu", method = RequestMethod.POST)
+    public CommonResult allocMenu(@RequestParam Long roleId, @RequestParam List<Long> menuIdList) {
+
+        if (!roleService.count(roleId)) {
+            return CommonResult.validateFailed("不存在，该角色编号！");
+        }
+
+        if (roleService.allocMenu(roleId, menuIdList)) {
+            return CommonResult.success();
+        }
+
+        return CommonResult.failed("服务器错误，请联系管理员！");
+    }
+
+    @ApiOperation("给角色分配资源")
+    @RequestMapping(value = "/allocResource", method = RequestMethod.POST)
+    public CommonResult allocResource(@RequestParam Long roleId, @RequestParam List<Long> resourceIdList) {
+
+        if (!roleService.count(roleId)) {
+            return CommonResult.validateFailed("不存在，该角色编号！");
+        }
+
+
+        if (roleService.allocResource(roleId, resourceIdList)) {
             return CommonResult.success();
         }
 
